72cfce0d8cf9acd87371c557b759a40300a2bc0c
DoumbiaCheickna <cheicknadevdot@gmail.com>
2025-11-28 05:37:37 +0000

    okayse

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -16,6 +16,8 @@ import {
   FaDownload,
   FaFilePdf
 } from "react-icons/fa";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
 
 export default function ContactPage() {
   const [formData, setFormData] = useState({
@@ -213,6 +215,8 @@ export default function ContactPage() {
   ];
 
   return (
+    <>
+    <Navbar/>
     <div className="min-h-screen bg-gradient-to-br from-slate-50 via-blue-50 to-cyan-50 py-8">
       <div className="max-w-7xl mx-auto px-4">
         {/* Header avec navigation */}
@@ -747,6 +751,8 @@ export default function ContactPage() {
         </div>
       </div>
     </div>
+    <Footer/>
+    </>
   );
 }
 
