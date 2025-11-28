e8198f53fae6f606030236187d523b9b0f3b3a0a
DoumbiaCheickna <cheicknadevdot@gmail.com>
2025-11-28 23:01:20 +0000

    okays

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,39 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-    reactStrictMode: true,
+// const nextConfig: NextConfig = {
+//     reactStrictMode: true,
+//   images: {
+//     domains: ["firebasestorage.googleapis.com"],
+//   },
+// };
+
+// export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone', // Important pour Render
+  trailingSlash: true,
   images: {
-    domains: ["firebasestorage.googleapis.com"],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'dmtc-sn.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.dmtc-sn.com',
+      }
+    ],
+    unoptimized: true, // Si vous avez des problèmes avec l'optimisation d'images
   },
-};
+  env: {
+    SITE_URL: process.env.NODE_ENV === 'production' 
+      ? 'https://dmtc-sn.com' 
+      : 'http://localhost:10000',
+  }
+}
 
-export default nextConfig;
+module.exports = nextConfig
